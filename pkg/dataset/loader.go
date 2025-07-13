5e29f86755265e2d4ad6db3343a783924f6871a8
Ghozi-Waridi <ghoziwaridi@gmail.com>
2025-07-13 16:05:31 +0700

    :star: ADD: Initial_Project

---
+++ pkg/dataset/loader.go
@@ -0,0 +1,68 @@
+package main
+
+import (
+	// "fmt"
+	"fmt"
+	"image"
+
+	// "image/color"
+	// "image/jpeg"
+	_ "image/jpeg"
+	_ "image/png"
+
+	// "io/fs"
+	"os"
+	// "path/filepath"
+)
+
+func LoadImage(path string) (image.Image, error) {
+
+	fileImage, errors := os.Open(path)
+
+	if errors != nil {
+		return nil, fmt.Errorf("failed to open image file %s: %w", path, errors)
+	}
+
+	defer fileImage.Close()
+	image, _, err := image.Decode(fileImage)
+
+	if err != nil {
+		return nil, fmt.Errorf("failed to decode image file %s: %w", path, err)
+	}
+
+	return image, nil
+}
+
+// Convert image to pixel array
+// type Pixel struct {
+// 	R, G, B uint8
+// }
+//
+// func (p Pixel) convertToArray(img image.Image) [][]Pixel {
+// 	width, height := img.Bounds().Dx(), img.Bounds().Dy()
+// 	pixels := make([][]Pixel, height)
+//
+// 	for y := 0; y < height; y++ {
+// 	pixels[y] = make([]Pixel, width)
+// 		for x := 0; x < width; x++ {
+// 				}
+// 	}
+//
+// 	return pixels
+// }
+
+func main() {
+	image, err := LoadImage("../../alya.jpg")
+
+	if err != nil {
+		fmt.Println("Error loading image:", err)
+		return
+	}
+
+	fmt.Println(image)
+
+	fmt.Println("Panjang ", image.Bounds().Dx())
+	fmt.Println("Lebar ", image.Bounds().Dy())
+
+	fmt.Println(image.At(0, 0))
+}
